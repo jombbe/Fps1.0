8dc8aa7536f95f9547f193775f7b265dd8578699
jombber <joonasrvnn@gmail.com>
2019-12-30 17:40:57 +0200

    Sword model chanced and animator script added

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,15 +8,22 @@ public class playerController : MonoBehaviour
     public float moveSpeed = 2.0f;
     public float Gravity = -9.81f;
     public float jumpHeight = 3.0f;
+    float previousHorizontalValue = 0;
+    float previousVerticalValue = 0;
 
     public CharacterController characterController;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    public Animator handAnim;
 
     Vector3 velocity;
     bool isGrounded;
 
+    public void Start()
+    {
+        handAnim = GameObject.Find("Hand").GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,7 +31,7 @@ public class playerController : MonoBehaviour
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -35,12 +42,27 @@ public class playerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * moveSpeed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * Gravity);
         }
 
         velocity.y += Gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
+
+
+        if (x != previousHorizontalValue || z != previousVerticalValue)
+        {
+            previousHorizontalValue = x;
+            previousVerticalValue = z;
+
+            handAnim.SetBool("Walk", true);    
+        }
+        else
+        {
+            
+        }
+
+
     }
 }
