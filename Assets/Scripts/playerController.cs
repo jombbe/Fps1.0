97ad105188bfed7603acca2aebfc76dde150556b
jombber <joonasrvnn@gmail.com>
2019-12-30 15:06:49 +0200

    fps1.0 first commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    public float rotateSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        CharacterController characterControl = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("W"))
+        {
+            walk();
+        }
+    }
+
+    public void walk()
+    {
+        
+    }
+}
